4dcb8f2196e059b42aaeb585c97a920e0765510e
19cae253 <shahanashhussain.2001@gmail.com>
2024-08-06 11:47:19 +0530

    "basic new"

---
+++ Basic/src/chitu1/Chitujav.java
@@ -0,0 +1,17 @@
+
+public void StudentName() {
+	System.out.println("sha");
+}
+public void StudentAge() {
+	System.out.println(22);	
+	}
+public static void main(String []args) {
+ Basic1 obj=new Basic1();
+ obj.StudentName();
+ Basic1 objj=new Basic1();
+ objj.StudentAge();
+
+}
+}
+
+
---
+++ Basic/src/java2/Basic1.java
@@ -0,0 +1,20 @@
+package java2;
+
+public class Basic1  {
+		public void StudentName() {
+			System.out.println("sha");
+		}
+		public void StudentAge() {
+			System.out.println(22);	
+			}
+	 public static void main(String []args) {
+		 Basic1 obj=new Basic1();
+		 obj.StudentName();
+		 Basic1 objj=new Basic1();
+		 objj.StudentAge();
+		
+	 }
+	}
+
+
+
